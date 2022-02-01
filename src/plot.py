18bdb0d7fe00f2d958e1c4b2d5bcf7ea6051fd0a
eerkela <eerkela42@gmail.com>
2022-02-01 01:34:52 -0800

    filling out package structure

---
+++ src/plot.py
@@ -0,0 +1,28 @@
+def configure_plot():
+    raise NotImplementedError()
+
+
+def configure_axes():
+    raise NotImplementedError()
+
+
+def add_axes():
+    raise NotImplementedError()
+
+
+def add_data():
+    raise NotImplementedError()
+
+
+def add_errors():
+    raise NotImplementedError()
+
+
+def add_fit():
+    raise NotImplementedError()
+
+
+class DynamicPlotter:
+
+    def __init__(self):
+        raise NotImplementedError()
